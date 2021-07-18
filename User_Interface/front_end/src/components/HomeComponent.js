511264836cbb17e950b4ada5153f35390a6c86ad
Ayat Mostafa <ayat.mostafa998@gmail.com>
2021-07-19 00:06:35 +0200

    Update HomeComponent.js

--- User_Interface/front_end/src/components/HomeComponent.js
+++ User_Interface/front_end/src/components/HomeComponent.js
@@ -20,104 +20,6 @@ const mapStateToProps = state => {
   }
 }
 
-
-
-// import VideoRecorder from 'react-video-recorder';
-// import { ReactMediaRecorder } from "react-media-recorder";
-// import {ZiggeoRecorder} from 'react-ziggeo'
-// import Webcam from "react-webcam";
-
-// import { RecordWebcam, useRecordWebcam, CAMERA_STATUS } from "react-record-webcam";
-// function RecordVideo(props) {
-//   const recordWebcam = useRecordWebcam({recordingLength:60});
-//   const saveFile = async () => {
-//     const blob = await recordWebcam.getRecording();
-//     // ...
-//   };
-//   return (
-//     <RecordWebcam isWebcamOn={true}
-//       // render={(props: WebcamRenderProps) => {
-//       //   return (
-//       //     <div>
-//       //       <h1>Component render prop demo</h1>
-//       //       <p>Camera status: {props.status}</p>
-//       //       <div>
-//       //         <button onClick={props.openCamera}>Open camera</button>
-//       //         <button onClick={props.retake}>Retake</button>
-//       //         <button onClick={props.start}>Start recording</button>
-//       //         <button onClick={props.stop}>Stop recording</button>
-//       //         <button onClick={props.download}>Download</button>
-//       //       </div>
-//       //     </div>
-//       //   );
-//       // }}
-//     />
-//   )
-// }
-// const [recorder, setRecorder] = useState(null);
-// useEffect(() => {
-//     if (recorder) {
-//         // DO stuff here
-//         recorder.on("any_event", function (rec) {  }, recorder);
-//         recorder.get("attribute_name");
-//     }
-// }, [recorder]);
-
-// Embedding (player/recorder instance) will be the first argument
-// const handleRecorderRecording = (embedding) => {
-//     console.log('Recorder onRecording');
-// };
-
-// const handleRecorderUploading = (embedding) => {
-//     console.log('Recorder uploading');
-// };
-
-
-// const videoJsOptions = {
-//   controls: true,
-//   bigPlayButton: false,
-//   fluid: true,
-//   width: 120,
-//   height: 120,
-//   aspectRatio: '3:1',
-//   autorecord: true,
-//   plugins: {
-//       /*
-//       // wavesurfer section is only needed when recording audio-only
-//       wavesurfer: {
-//           backend: 'WebAudio',
-//           waveColor: '#36393b',
-//           progressColor: 'black',
-//           debug: true,
-//           cursorWidth: 1,
-//           msDisplayMax: 20,
-//           hideScrollbar: true,
-//           displayMilliseconds: true,
-//           plugins: [
-//               // enable microphone plugin
-//               WaveSurfer.microphone.create({
-//                   bufferSize: 4096,
-//                   numberOfInputChannels: 1,
-//                   numberOfOutputChannels: 1,
-//                   constraints: {
-//                       video: false,
-//                       audio: true
-//                   }
-//               })
-//           ]
-//       },
-//       */
-//       record: {
-//           audio: true,
-//           video: true,
-//           maxLength: 20,
-//           debug: true,
-//       }
-//   }
-// };
-
-
-
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -134,13 +36,6 @@ class Home extends Component {
   ];
   this.handleChange = this.handleChange.bind(this);
   this.getQuestion  = this.getQuestion.bind(this);
-  this.setSeconds = this.setSeconds.bind(this);
-  }
-  setSeconds(s){
-    // if(this.state.seconds > 0)
-    // {
-
-    // }
   }
   handleChange(event) {
     const { name, value } = event.target;
@@ -246,3 +141,98 @@ class Home extends Component {
   }
 }
 export default withRouter(connect(mapStateToProps)(Home));
+
+
+// import VideoRecorder from 'react-video-recorder';
+// import { ReactMediaRecorder } from "react-media-recorder";
+// import {ZiggeoRecorder} from 'react-ziggeo'
+// import Webcam from "react-webcam";
+
+// import { RecordWebcam, useRecordWebcam, CAMERA_STATUS } from "react-record-webcam";
+// function RecordVideo(props) {
+//   const recordWebcam = useRecordWebcam({recordingLength:60});
+//   const saveFile = async () => {
+//     const blob = await recordWebcam.getRecording();
+//     // ...
+//   };
+//   return (
+//     <RecordWebcam isWebcamOn={true}
+//       // render={(props: WebcamRenderProps) => {
+//       //   return (
+//       //     <div>
+//       //       <h1>Component render prop demo</h1>
+//       //       <p>Camera status: {props.status}</p>
+//       //       <div>
+//       //         <button onClick={props.openCamera}>Open camera</button>
+//       //         <button onClick={props.retake}>Retake</button>
+//       //         <button onClick={props.start}>Start recording</button>
+//       //         <button onClick={props.stop}>Stop recording</button>
+//       //         <button onClick={props.download}>Download</button>
+//       //       </div>
+//       //     </div>
+//       //   );
+//       // }}
+//     />
+//   )
+// }
+// const [recorder, setRecorder] = useState(null);
+// useEffect(() => {
+//     if (recorder) {
+//         // DO stuff here
+//         recorder.on("any_event", function (rec) {  }, recorder);
+//         recorder.get("attribute_name");
+//     }
+// }, [recorder]);
+
+// Embedding (player/recorder instance) will be the first argument
+// const handleRecorderRecording = (embedding) => {
+//     console.log('Recorder onRecording');
+// };
+
+// const handleRecorderUploading = (embedding) => {
+//     console.log('Recorder uploading');
+// };
+
+
+// const videoJsOptions = {
+//   controls: true,
+//   bigPlayButton: false,
+//   fluid: true,
+//   width: 120,
+//   height: 120,
+//   aspectRatio: '3:1',
+//   autorecord: true,
+//   plugins: {
+//       /*
+//       // wavesurfer section is only needed when recording audio-only
+//       wavesurfer: {
+//           backend: 'WebAudio',
+//           waveColor: '#36393b',
+//           progressColor: 'black',
+//           debug: true,
+//           cursorWidth: 1,
+//           msDisplayMax: 20,
+//           hideScrollbar: true,
+//           displayMilliseconds: true,
+//           plugins: [
+//               // enable microphone plugin
+//               WaveSurfer.microphone.create({
+//                   bufferSize: 4096,
+//                   numberOfInputChannels: 1,
+//                   numberOfOutputChannels: 1,
+//                   constraints: {
+//                       video: false,
+//                       audio: true
+//                   }
+//               })
+//           ]
+//       },
+//       */
+//       record: {
+//           audio: true,
+//           video: true,
+//           maxLength: 20,
+//           debug: true,
+//       }
+//   }
+// };
